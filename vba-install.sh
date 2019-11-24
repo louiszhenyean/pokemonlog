9ac45ac4889f5daf3d9999824d0995412d1a211a
louiszhenyean <louiszhenyean@gmail.com>
2019-11-25 07:44:54 +0800

    Update vba-install.sh

--- vba-install.sh
+++ vba-install.sh
@@ -4,3 +4,5 @@ wget http://ftp.us.debian.org/debian/pool/main/v/visualboyadvance/visualboyadvan
 sudo apt install libgtkmm-2.4-1v5 -y
 sudo dpkg -i visualboyadvance_1.8.0.dfsg-4_amd64.deb
 sudo dpkg -i visualboyadvance-gtk_1.8.0.dfsg-4_amd64.deb
+rm visualboyadvance_1.8.0.dfsg-4_amd64.deb
+rm visualboyadvance-gtk_1.8.0.dfsg-4_amd64.deb
