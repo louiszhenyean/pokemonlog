638ddcba939c5c7e8ad40d736fd46c707c1acd78
Yean <louiszhenyean@gmail.com>
2020-10-01 10:33:35 +0800

    Remove ThunderYellow

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # pokemonlog
 Play Pokémon Gameboy Games on Debian!!
 
+# Install DeSmuME for NDS
+```
+sudoapt install desmume
+```
+
 # Installing VBA-gtk
+```
 chmod a+x vba-install.sh  
 ./vba-install.sh  
+```
 
 # Fixing White Screen and Screen Size
+```
 Options > Emulator > Save type > Flash 128K  
 Options > Video > 6x  
+```
--- vba-install.sh
+++ vba-install.sh
@@ -1,7 +1,5 @@
-sudo apt remove visualboyadvance -y
 wget http://ftp.us.debian.org/debian/pool/main/v/visualboyadvance/visualboyadvance_1.8.0.dfsg-4_amd64.deb
 wget http://ftp.us.debian.org/debian/pool/main/v/visualboyadvance/visualboyadvance-gtk_1.8.0.dfsg-4_amd64.deb
-sudo apt install libgtkmm-2.4-1v5 -y
 sudo dpkg -i visualboyadvance_1.8.0.dfsg-4_amd64.deb
 sudo dpkg -i visualboyadvance-gtk_1.8.0.dfsg-4_amd64.deb
 rm visualboyadvance_1.8.0.dfsg-4_amd64.deb
